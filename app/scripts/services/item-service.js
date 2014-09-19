3d639e856c220947d9d7fbcdb73918c9b9dd5fb2
lzhangwei <zhangwei_999@foxmail.com>
2014-09-19 13:08:27 +0800

    update item service

--- app/scripts/services/item-service.js
+++ app/scripts/services/item-service.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('letusgoApp')
-  .service('ItemService', function ($http,$q) {
+  .service('ItemService', function ($http) {
 
     this.loadAllItems = function(callback){
       $http.get('/api/items')
