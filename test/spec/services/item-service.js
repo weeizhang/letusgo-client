3f006f5816b69288891a339440ff7153560234ff
lzhangwei <zhangwei_999@foxmail.com>
2014-09-25 09:33:17 +0800

    update load items test

--- test/spec/services/item-service.js
+++ test/spec/services/item-service.js
@@ -1,13 +1,13 @@
 'use strict';
 
 describe('Service: itemService', function () {
-  var itemService, $http,items;
+  var itemService, $httpBackend,items;
 
   beforeEach(function () {
     module('letusgoApp');
     inject(function ($injector) {
       itemService = $injector.get('ItemService');
-      $http = $injector.get('$http');
+      $httpBackend = $injector.get('$httpBackend');
     });
     var item1 = {'barcode': 'ITEM000000', 'name': '可口可乐', 'unit': '瓶', 'price': 3.00, 'category': '饮料'};
     var item2 = {'barcode': 'ITEM000001', 'name': '雪碧', 'unit': '瓶', 'price': 3.00, 'category': '饮料'};
@@ -16,16 +16,19 @@ describe('Service: itemService', function () {
     var item5 = {'barcode': 'ITEM000004', 'name': '电池', 'unit': '个', 'price': 2.00, 'category': '生活用品'};
     var item6 = {'barcode': 'ITEM000005', 'name': '方便面', 'unit': '袋', 'price': 4.50, 'category': '食品'};
     items = [item1, item2, item3, item4, item5, item6];
+    $httpBackend.expectGET('/api/items').
+      respond(items);
   });
 
-  it('should return items', function() {
+  it('should make a GET request and return items', function() {
     var callback = jasmine.createSpy('callback');
     callback({
       items: items
     });
-
-    itemService.loadAllItems(callback);
-
+    $httpBackend.expectGET('/api/items');
+    itemService.loadAllItems(callback, function(){
+      $httpBackend.flush();
+    });
     expect(callback).toHaveBeenCalledWith(jasmine.objectContaining({
       items: items
     }));
