fbfcd18ca58bdcde6fb827f9554c152cd634c6f8
lzhangwei <zhangwei_999@foxmail.com>
2014-09-26 19:41:35 +0800

    add call add cart item test

--- test/spec/controllers/list.js
+++ test/spec/controllers/list.js
@@ -32,8 +32,6 @@ describe('Controller: ListCtrl', function () {
     var item6 = {'barcode': 'ITEM000005', 'name': '方便面', 'unit': '袋', 'price': 4.50, 'category': '食品'};
     items = [item1, item2, item3, item4, item5, item6];
 
-    createController();
-
   });
 
   it('should emit to parent controller when come in list', function () {
@@ -50,6 +48,14 @@ describe('Controller: ListCtrl', function () {
     itemService.loadAllItems(function (data) {
       expect($scope.items).toEqual(data);
     });
+  });
+
+  it('should call addCartItem in cartService', function () {
+    spyOn(cartService, 'addCartItem');
+    createController();
+    var item = {'barcode': 'ITEM000000', 'name': '可口可乐', 'unit': '瓶', 'price': 3.00, 'category': '饮料'};
+    $scope.addCartItem(item);
+    expect(cartService.addCartItem).toHaveBeenCalled();
   });
 
 });
