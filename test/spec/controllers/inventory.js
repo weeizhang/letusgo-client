62ff1021c12f68a0ee38abce1957edd705d6cab4
lzhangwei <zhangwei_999@foxmail.com>
2014-09-26 19:54:16 +0800

    update inventroy controller test

--- test/spec/controllers/inventory.js
+++ test/spec/controllers/inventory.js
@@ -2,7 +2,7 @@
 
 xdescribe('Controller: InventoryCtrl', function () {
 
-  var $scope, cartService, createController;
+  var $scope, cartService, createController, cartItems;
 
   beforeEach(function () {
     module('letusgoApp');
@@ -22,33 +22,21 @@ xdescribe('Controller: InventoryCtrl', function () {
       };
     });
 
-    createController();
+    var cartItem1 = {"item":{"id":1,"barcode":"ITEM000000","name":"可口可乐","unit":"瓶","price":3,"category":"饮料"},"num":2};
+    var cartItem2 = {"item":{"id":2,"barcode":"ITEM000001","name":"雪碧","unit":"瓶","price":3,"category":"饮料"},"num":1};
+    var cartItem3 = {"item":{"id":3,"barcode":"ITEM000002","name":"苹果","unit":"斤","price":5.5,"category":"水果"},"num":1};
+    cartItems = [cartItem1, cartItem2, cartItem3];
 
   });
 
   it('should call getCartItem in cartService and return to cartItemList', function () {
-    spyOn(cartService, 'getCartItem');
+    spyOn(cartService, 'getCartItem').and.callFake(function (callback) {
+      callback(cartItems);
+    });
     createController();
-    var cartItems = cartService.getCartItem();
-    expect($scope.cartItemList).toEqual(cartItems);
+    itemService.loadAllItems(function (data) {
+      expect($scope.cartItemList).toEqual(cartItems);
+    });
   });
-
-  it('should call totalPrice in cartService and return to totalPrice', function () {
-    spyOn(cartService, 'totalPrice');
-    var totalPrice = cartService.totalPrice($scope.cartItemList);
-    expect($scope.totalPrice).toEqual(totalPrice);
-  });
-
-  it('should call cleanCart in cartService when click pay button', function () {
-    spyOn(cartService, 'cleanCart');
-    spyOn($scope, '$emit');
-    $scope.okPayClick();
-    expect(cartService.cleanCart).toHaveBeenCalled();
-  });
-
-  it('should emit to parent controller when click pay button', function () {
-    spyOn($scope, '$emit');
-    $scope.okPayClick();
-    expect($scope.$emit).toHaveBeenCalledWith('to-parent-changeamounts');
-  });
+  
 });
