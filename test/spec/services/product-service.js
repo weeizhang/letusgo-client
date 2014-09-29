101f427b481d468fbcadabf0c6191286e5680c3c
lzhangwei <zhangwei_999@foxmail.com>
2014-09-29 22:48:46 +0800

    update product service test

--- test/spec/services/product-service.js
+++ test/spec/services/product-service.js
@@ -10,9 +10,9 @@ describe('Service: ProductService', function () {
     });
 
     productList = [
-      {'barcode': 'ITEM000000', 'name': '可口可乐', 'unit': '瓶', 'price': 3.00, 'category': '饮料'},
-      {'barcode': 'ITEM000001', 'name': '雪碧', 'unit': '瓶', 'price': 3.00, 'category': '饮料'},
-      {'barcode': 'ITEM000002', 'name': '苹果', 'unit': '斤', 'price': 5.50, 'category': '水果'}
+      {'id':1, 'barcode': 'ITEM000000', 'name': '可口可乐', 'unit': '瓶', 'price': 3.00, 'category': '饮料'},
+      {'id':2, 'barcode': 'ITEM000001', 'name': '雪碧', 'unit': '瓶', 'price': 3.00, 'category': '饮料'},
+      {'id':3, 'barcode': 'ITEM000002', 'name': '苹果', 'unit': '斤', 'price': 5.50, 'category': '水果'}
     ];
   });
 
@@ -24,6 +24,15 @@ describe('Service: ProductService', function () {
     $httpBackend.expectGET('/api/items').respond(200, productList);
     productService.getAllProductInfo(function (data) {
       expect(data.length).toBe(3);
+    });
+    $httpBackend.flush();
+  });
+
+  it('should return the product info by id', function () {
+    var product = {'id':1, 'barcode': 'ITEM000000', 'name': '可口可乐', 'unit': '瓶', 'price': 3.00, 'category': '饮料'};
+    $httpBackend.expectGET('/api/items/1').respond(200, product);
+    productService.getProductInfoById(1, function (data) {
+      expect(data).toEqual(product);
     });
     $httpBackend.flush();
   });
